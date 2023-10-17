4bb2e51b450dafe1a76bdba6009554876cebd933
Valeria Palacios <79058926+ValeSPalacios@users.noreply.github.com>
2023-10-16 21:05:15 -0300

    #2 add files

---
+++ src/controller.js
@@ -0,0 +1,28 @@
+import {pool} from './database.js';
+
+class LibroController {
+    async getAll(req, res){
+        const [result] = await pool.query('SELECT * FROM libros');
+        res.json(result);
+    }
+
+    async add(req,res){
+        const libro=req.body;
+        const [result] = await pool.query(`INSERT INTO Libros(nombre,autor,categoria,anio_publicacion,isbn) VALUES (?, ?, ?, ? ,?)`, [libro.nombre, libro.autor, libro.categoria, libro.anio_publicación, libro.isbn]);
+        res.json ({"ID insertado": result.insertId});
+    }
+
+    async getOne(req,res){
+        const libro=req.body;
+        const [result] = await pool.query(`SELECT * FROM Libros WHERE id=?`, [libro.id]);
+        if (result.length > 0){
+          res.json (result);
+        }
+        else {
+          res.json({"Error": "No existe un libro con el ID insertado."});
+        }
+    
+    }
+
+}
+export const libro = new LibroController();
