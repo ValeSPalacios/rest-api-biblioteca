4baeaf5ee9883c23fae137f82df57d2d2ca6feaf
Valeria Palacios <79058926+ValeSPalacios@users.noreply.github.com>
2023-10-16 22:54:17 -0300

    #4 update errors

--- src/controller.js
+++ src/controller.js
@@ -1,20 +1,22 @@
 import {pool} from './database.js';
 
 class LibroController {
-    async getAll (req, res){
-        const [result] = await pool.query('SELECT * FROM libros');
-        res.json(result);
+    async getAll (res){
+      const [result] = await pool.query('SELECT * FROM libros');
+      res.json(result);
     }
 
-    async add (req,res){
-        const libro=req.body;
-        const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, anio_publicacion, ISBN) VALUES (?, ?, ?, ? ,?)`, [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.isbn]);
-        res.json ({"ID insertado": result.insertId});
+    async add (req, res){
+      const libro = req.body;
+      const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, anio_publicacion, ISBN) VALUES (?, ?, ?, ? ,?)`, [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.isbn]);
+      res.json ({"ID insertado": result.insertId});
     }
 
-    async getOne (req,res){
-      const libro=req.body;
-      const [result] = await pool.query(`SELECT * FROM Libros WHERE id=?`, [libro.id]);
+    async getOne (req, res){
+      const libro = req.body;
+      const id_libro = parseInt(libro.id);
+      console.log(libro);
+      const [result] = await pool.query(`SELECT * FROM Libros WHERE id=?`, [id_libro]);
       if (result.length > 0){
         res.json (result);
       }
