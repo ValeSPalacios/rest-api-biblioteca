2f216b9feed2f31660e29fcb21230f0ea0044cab
Valeria Palacios <79058926+ValeSPalacios@users.noreply.github.com>
2023-10-23 13:57:42 -0300

    #7 add delete

--- src/controller.js
+++ src/controller.js
@@ -8,13 +8,15 @@ class LibroController {
 
     async add (req, res){
       const libro = req.body;
-      const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, anio_publicacion, ISBN) VALUES (?, ?, ?, ? ,?)`, [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.isbn]);
+      const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, anio_publicacion, ISBN)
+                        VALUES (?, ?, ?, ? ,?)`, [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.ISBN]);
       res.json ({"ID insertado": result.insertId});
     }
 
     async getOne (req, res){
-      const libro = req.body;
-      const id_libro = parseInt(libro.id);
+      //const libro = req.body;
+      //const libro = req.params.id;
+      const id_libro = parseInt(req.params.id);
       console.log(libro);
       const [result] = await pool.query(`SELECT * FROM Libros WHERE id=?`, [id_libro]);
       if (result.length > 0){
@@ -31,6 +33,24 @@ class LibroController {
       const [result] = await pool.query( `UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), anio_publicacion=(?), ISBN=(?) WHERE id=(?)`,
                         [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.isbn, id_libro]);
       res.json({"Libro actualizado": result.changedRows});
+    }
+
+    async delete(req, res) {
+      try {
+        const libro = req.body;
+        if (!Number.isInteger(libro.id)) throw("Por favor, ingrese un entero positivo para el id");
+        const [result] = await pool.query(`DELETE FROM Libro WHERE id=(?)`, [libro.id]);
+
+        if (result.affectedRows>0) {
+          res.json(`El libro con id ${libro.id} ha sido eliminado`);
+        } else {
+          res.json(`No existe el libro con id ${libro.id} en la base de datos`);
+        }
+
+      } catch(e) {
+        console.log(e);
+        res.status(404).json({"Error": e});
+      }
     }
 }
 export const libro = new LibroController();
